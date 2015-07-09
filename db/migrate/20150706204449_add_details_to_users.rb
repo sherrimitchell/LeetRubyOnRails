5c864585e94b9361ddfc56762bda1d5bae20fef1
Sherri Mitchell <rsamitchell@att.net>
2015-07-09 16:26:17 -0400

    Fix user migration and update schema.

--- db/migrate/20150706204449_add_details_to_users.rb
+++ db/migrate/20150706204449_add_details_to_users.rb
@@ -1,7 +1,5 @@
 class AddDetailsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :avatar_url, :string
-    add_column :users, :linked_in_url, :string
-    add_column :users, :phone_number, :string
   end
 end
